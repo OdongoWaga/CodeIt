a6338fa2164a8f0b3a3079c4525e6383495c3e70
Waga Odongo <wagaodongo@gmail.com>
2019-02-02 16:55:28 +0300

    good
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CodeIt a platform for sharing code.' });
+  res.render('index', { title: 'CodeIt - a platform for sharing code.' });
 });
 
-router.get('/about', function(req, res, next){
-  res.render('about', {title: 'CodeIt a platform for sharing code.'});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'CodeIt - a platform for sharing code.'});
 });
+
 router.route('/contact')
-.get( function(req, res, next){
- res.render('contact', {title: 'CodeIt a platform for sharing code.'})
-})
-.post(function(req, res, next){
- req.checkBody('name', "Empty Name").notEmpty();
- req.checkBody('email', "Invalid email").isEmail();
- req.checkBody('message', "Empty Message").notEmpty();
- var errors = res.validationErrors();
+  .get(function(req, res, next) {
+    res.render('contact', { title: 'CodeIt - a platform for sharing code.'});
+  })
+  .post(function(req, res, next) {
+    req.checkBody('name', 'Empty name').notEmpty();
+    req.checkBody('email', 'Invalid email').isEmail();
+    req.checkBody('message', 'Empty message').notEmpty();
+    var errors = req.validationErrors();
 
-    
-res.render('thank', { title: 'CodeIt a platform for sharing code.' });
-});
+    if(errors) {
+      res.render('contact', {
+        title: 'CodeIt - a platform for sharing code.',
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+        errorMessages: errors
+      });
+    } else {
+      res.render('thank', { title: 'CodeIt - a platform for sharing code.'});
+    }
+  });
 
 module.exports = router;
