5170e446201217c4c90da5fc0e429177f2ca19ef
Waga Odongo <wagaodongo@gmail.com>
2019-02-02 16:08:46 +0300

    good
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Leo' });
+  res.render('index', { title: 'CodeIt a platform for sharing code.' });
+});
+
+router.get('/about', function(req, res, next){
+  res.render('about', {title: 'CodeIt a platform for sharing code.'});
 });
 
 module.exports = router;
