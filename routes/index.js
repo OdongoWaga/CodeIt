e4db5a090a363347bed012ba3cbe44f504e97ead
Waga Odongo <wagaodongo@gmail.com>
2019-02-02 16:19:46 +0300

    contact
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,12 @@ router.get('/', function(req, res, next) {
 router.get('/about', function(req, res, next){
   res.render('about', {title: 'CodeIt a platform for sharing code.'});
 });
+router.route('/contact')
+.get( function(req, res, next){
+ res.render('contact', {title: 'CodeIt a platform for sharing code.'})
+})
+.post(function(req, res, next){
+res.render('thank', { title: 'CodeIt a platform for sharing code.' });
+});
 
 module.exports = router;
