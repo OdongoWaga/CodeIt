1d47fd669f2c3fac6cdc41d8935d8c9b96f68bb1
Waga Odongo <wagaodongo@gmail.com>
2019-02-03 21:15:19 +0300

    login
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- passport.js
+++ passport.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.serializeUser(function (user, done) {
   done(null, user._id);
@@ -30,5 +31,43 @@ passport.use(new LocalStrategy({
 
       return done(null, user);
     })
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: '372673533520396',
+    clientSecret: '16fbabda08ca9b858278eb6e48a497b2',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'email']
+  },
+  function(token, refreshToken, profile, done) {
+    User.findOne({'facebookId': profile.id}, function(err, user) {
+      if (err) return done(err);
+
+      if (user) {
+        return done(null, user);
+      } else {
+        User.findOne({email: profile.emails[0].value}, function (err, user) {
+          if (user) {
+            user.facebookId = profile.id
+            return user.save(function (err) {
+              if (err) return done(null, false, { message: "Can't save user info"});
+              return done(null, user);
+            })
+          }
+
+          var user = new User();
+          user.name = profile.displayName;
+          user.email = profile.emails[0].value;
+          user.facebookId = profile.idea
+          user.save(function (err) {
+            if (err) return done(null, false, { message: "Can't save user info"});
+            return done(null, user);
+          });
+        })
+      }
+
+
+    });
   }
 ));
