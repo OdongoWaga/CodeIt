96a05cb630deea994da127dd7346b782e9bdd18f
Waga Odongo <wagaodongo@gmail.com>
2019-02-03 15:10:38 +0300

    4
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -50,4 +50,14 @@ router.route('/contact')
     }
   });
 
+
+  router.get('/register', function(req, res, next) {
+    res.render('register', {title:'Register' });
+  });
+  
+  router.get('/login', function(req, res, next) {
+    res.render('login', {title:'Login' });
+  });
+  
+
 module.exports = router;
